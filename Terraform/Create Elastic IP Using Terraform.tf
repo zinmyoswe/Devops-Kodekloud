b91cf9804cf54fc30e1e640169e2c3ec8a067988
ZIN MYO SWE <overmidnight12@gmail.com>
2025-10-17 09:16:35 +0700

    Create Elastic IP Using Terraform

---
+++ Terraform/Create Elastic IP Using Terraform.tf
@@ -0,0 +1,15 @@
+The Nautilus DevOps team is strategizing the migration of a portion of their infrastructure to the AWS cloud. Recognizing the scale of this undertaking, they have opted to approach the migration in incremental steps rather than as a single massive transition.
+To achieve this, they have segmented large tasks into smaller, more manageable units. This granular approach enables the team to execute the migration in gradual phases, ensuring smoother implementation and minimizing disruption to ongoing operations.
+By breaking down the migration into smaller tasks, the Nautilus DevOps team can systematically progress through each stage, allowing for better control, risk mitigation, and optimization of resources throughout the migration process.
+For this task, allocate an Elastic IP address named xfusion-eip using Terraform.
+The Terraform working directory is /home/bob/terraform. Create the main.tf file (do not create a different .tf file) to accomplish this task.
+Note: Right-click under the EXPLORER section in VS Code and select Open in Integrated Terminal to launch the terminal.
+
+Solution:
+
+resource "aws_eip" "xfusion-eip" {
+  domain   = "vpc"
+  tags = {
+        Name        = "xfusion-eip"
+      }
+}
