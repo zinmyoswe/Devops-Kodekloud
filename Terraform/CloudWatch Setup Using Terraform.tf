6485b7bbeba7a88f8e2e2ceb9f565538a1fec681
ZIN MYO SWE <overmidnight12@gmail.com>
2025-10-26 23:49:50 +0700

    CloudWatch Setup Using Terraform

---
+++ Terraform/CloudWatch Setup Using Terraform.tf
@@ -0,0 +1,9 @@
+The Nautilus DevOps team needs to set up CloudWatch logging for their application. They need to create a CloudWatch log group and log stream with the following specifications:
+
+1) The log group name should be devops-log-group.
+
+2) The log stream name should be devops-log-stream.
+
+Use Terraform to create the CloudWatch log group and log stream. The Terraform working directory is /home/bob/terraform. Create the main.tf file (do not create a different .tf file) to accomplish this task.
+
+Note: Right-click under the EXPLORER section in VS Code and select Open in Integrated Terminal to launch the terminal.
